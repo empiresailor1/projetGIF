dddd9e543840f8c7965e301bc595e85b77d31e1b
DeeWann450 <45037757+DeeWann450@users.noreply.github.com>
2018-11-14 14:08:13 +0100

    Add files via upload

---
+++ Projet.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+int main()
+{	
+	int c=0;
+	while (c!=1)
+	{
+		printf("Menu\n");
+		printf("1-Ajouter Ville \n2-Modifier ville\n3-Supprimer Ville\n4-Ajouter nouvelle livraison dans une ville\n5-Supprimer livraison\n6-Modifier livraison\n7-Recherche (Ville, capacite)\n8-Recherche(capacite)\n9-Transfert (chauffeur, ville1, ville2)");
+		int f;
+		scanf("%d",&f);
+		if (f==1)
+		{
+			
+		}
+		if (f==2)
+		{
+			
+		}
+		if (f==3)
+		{
+			
+		}
+		if (f==4)
+		{
+			
+		}
+		if (f==5)
+		{
+			
+		}
+		if (f==6)
+		{
+			
+		}
+		if (f==7)
+		{
+			
+		}
+		if (f==8)
+		{
+			
+		}
+		if (f==9)
+		{
+			
+		}
+	}
+}
